3742d1a5fda3773867cd2bc9bb901fdd78325656
dave <david.hache@desjardins.com>
2019-10-02 13:05:32 -0400

    initial edit of tf

---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "vmware_vcenteruser" {}
+variable "secret_vcenterpass" {}
+variable "vmware_vcenterhost" {}
+variable "vmware_datacenter" {}
+variable "vmware_clusters" {}
+variable "vmware_datastore_per" {}
+variable "env" {}
